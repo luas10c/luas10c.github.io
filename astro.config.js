5ac65086701bb68c49fb862f1bf558c7e5ab0db8
Luciano Alves <luas10c@gmail.com>
2024-09-28 13:27:12 -0300

    chore: first commit

---
+++ astro.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path'
+
+const config = defineConfig({
+  output: 'static',
+  experimental: {
+    svg: true
+  },
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '#': path.join(import.meta.dirname, 'src')
+      }
+    }
+  },
+  server: {
+    port: 3000
+  }
+})
+
+export default config
