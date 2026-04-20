0877929947bcb05c9bc3a3dc76901879bbf31bf1
Luciano Alves <luas10c@gmail.com>
2026-04-20 15:16:12 -0300

    feat: add section certificates

---
+++ src/data/certificates.ts
@@ -0,0 +1,30 @@
+export const certificates = [
+  {
+    title: 'DDD no Node.js',
+    issuer: 'Rocketseat',
+    date: new Date('2025-10-25'),
+    url: 'https://app.rocketseat.com.br/certificates/e5230351-a147-4f62-9dab-8a827ce20b74',
+    credentialId: '16737bad-3fd6-4fa1-bfaa-ed320908fcd3'
+  },
+  {
+    title: 'Fundamentos do React.js',
+    issuer: 'Rocketseat',
+    date: new Date('2025-10-26'),
+    url: 'https://app.rocketseat.com.br/certificates/838fb640-91ed-4ab9-9892-2daf804225a5',
+    credentialId: '838fb640-91ed-4ab9-9892-2daf804225a5'
+  },
+  {
+    title: 'Fundamentos do Node.js',
+    issuer: 'Rocketseat',
+    date: new Date('2025-10-25'),
+    url: 'https://app.rocketseat.com.br/certificates/16737bad-3fd6-4fa1-bfaa-ed320908fcd3',
+    credentialId: 'e5230351-a147-4f62-9dab-8a827ce20b74'
+  },
+  {
+    title: 'JavaScript do básico ao avançado (c/ Node.js e projetos)',
+    issuer: 'Udemy',
+    date: new Date('2024-01-15'),
+    url: 'https://www.udemy.com/certificate/UC-2a47fd13-b4c1-4b4c-989f-53d1f72abe27',
+    credentialId: 'UC-2a47fd13-b4c1-4b4c-989f-53d1f72abe27'
+  }
+] as const
